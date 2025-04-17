703be679565ff003831291b52fb0c727ca31cf5e
herman-verhelst <herman.verhelst01@gmail.com>
2025-04-17 13:44:43 +0200

    feat: add vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './assets/styles/main.scss'
 import './assets/styles/tailwind.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from "@/router/index.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Welcome from "@/views/Welcome.vue";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            component: Welcome
+        },
+        {
+            path: '/welcome',
+            component: Welcome
+        }
+    ]
+});
+
+export default router
